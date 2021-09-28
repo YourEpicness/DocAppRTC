418bed6f67ae3bcb0dc9f9641fbfcc38880b809a
Julaam Diop <jeebe.diop@gmail.com>
2021-09-28 00:02:02 -0500

    Use prettier to format code

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,20 @@
-'use strict'
+"use strict";
 
 const $self = {
-	constraints: {
-		audio: false,
-		video: true,
-	},
-}
+  rtcConfig: null,
+  constraints: {
+    audio: false,
+    video: true,
+  },
+};
 
-requestUserMedia($self.constraints)
+const $peer = {
+  connection: new RTCPeerConnection(),
+};
+requestUserMedia($self.constraints);
 
 async function requestUserMedia(constraints) {
-	const video = document.querySelector('#self')
-	$self.stream = await navigator.mediaDevices.getUserMedia(constraints)
-	video.srcObject = $self.stream
+  const video = document.querySelector("#self");
+  $self.stream = await navigator.mediaDevices.getUserMedia(constraints);
+  video.srcObject = $self.stream;
 }
